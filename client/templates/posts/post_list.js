d13614f35bd14f48c2f8c3b60f87192278fab9fd
zaoldyeck <zaoldyeck9970@gmail.com>
2015-05-12 00:05:36 +0800

    Fade items in when they are drawn.

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,5 +1,11 @@
 Template.postsList.onRendered(function () {
     this.find('.wrapper')._uihooks = {
+        insertElement: function (node, next) {
+            $(node)
+                .hide()
+                .insertBefore(next)
+                .fadeIn();
+        },
         moveElement: function (node, next) {
             var $node = $(node), $next = $(next);
             var oldTop = $node.offset().top;
@@ -32,6 +38,11 @@ Template.postsList.onRendered(function () {
             // 动画，重置所有元素的 top 坐标为 0
             $node.addClass('animate').css('top', 0);
             $inBetween.addClass('animate').css('top', 0);
+        },
+        removeElement: function (node) {
+            $(node).fadeOut(function () {
+                $(this).remove();
+            });
         }
     }
 });
